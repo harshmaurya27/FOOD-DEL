842917a5c08301cc02081021dbb052eb26e1eb7b
Harsh Maurya <137767009+harshmaurya27@users.noreply.github.com>
2024-08-05 16:06:34 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-awri.onrender.com";
   return (
     <div>
       <ToastContainer />
